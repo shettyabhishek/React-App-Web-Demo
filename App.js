b04cf6b7ecb918006a51fbb16dd47e0d59938cd9
Abhishek H Shetty <shettyabhishek829@yahoo.in>
2017-07-22 18:33:34 +0530

    Changes for integrating Redux

---
+++ App.js
@@ -0,0 +1,53 @@
+/**
+    File name: App.js
+    Author: Abhishek H Shetty
+    This component is the main container of the web application.
+**/
+
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Headercomp from './components/headerComp';
+import ContentComp from './components/mainContentComp';
+import Footercomp from './components/footerComp';
+import Home from './components/homePageComponent';
+import RctFtrs from './components/reactFeatureComp';
+import ReduxRcts from './components/reduxReactComp';
+import Profile from './components/profileComp';
+import { Provider } from 'react-redux';
+import createTodoStore from './components/todoStore';
+
+const store = createTodoStore();
+
+class App extends Component{
+    /** Adding a constructor,its the first life cycle function that gets called.
+        Since the function is called even before render, this can be used for
+        most of the initializations for the component.
+    **/
+    constructor(props){
+        super(props);
+        /** Declaring a state **/
+        this.state = {
+            message : 'Welcome to React App Demo'
+        }
+    }
+
+    /** Every component must implement the render function, every time the state changes the render is called **/
+    render() {
+        return (
+              <div className='appMain'>
+                  <Headercomp></Headercomp>
+                  <ContentComp>
+                      <Route exact path="/" component={Home}></Route>
+                      <Route path="/rctFtrs" name="rctFtrs" component={RctFtrs}></Route>
+                      <Provider store={store}>
+                          <Route path="/rdxRcts" name="rdxRcts" component={ReduxRcts}></Route>
+                      </Provider>
+                      <Route path="/profile" name="profile" component={Profile}></Route>
+                  </ContentComp>
+                  <Footercomp></Footercomp>
+              </div>
+        );
+    }
+}
+
+export default App;
