74c81dad2183083310920a7658f6f3f988504e36
Abhishek H Shetty <shettyabhishek829@yahoo.in>
2017-07-25 10:23:38 +0530

    Changes for integrating - Todo Component
    Implemented using React and redux

--- src/js/components/reduxReactComp.js
+++ src/js/components/reduxReactComp.js
@@ -6,14 +6,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as todoAction from './actions/todoActions';
-import {deleteTodo} from '../../img/deleteRed.png'; // relative path to image
+//import {deleteTodo} from '../../img/deleteRed.png'; // relative path to image
 
 
 // Maps state from store to props, this function basically maps the state props from the store
 // to the props. So that can be mapped to the components as input data to the component
 const mapStateToProps = (state, ownProps) => {
     return {
-        todos: state.todos
+        todos: state.todos === undefined ? [] : state.todos,
+        lastIndex: state.lastIndex === undefined ? 0 : state.lastIndex
     }
 };
 
@@ -22,22 +23,31 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         // You can now say this.props.createBook
-        createToDo: todo => {dispatch(todoAction.addItem(todo));}
+        createToDo: todo => {dispatch(todoAction.addItem(todo));},
+        removeToDoItem: todo => {dispatch(todoAction.removeItem(todo));},
+        selectTodoItem: todo => {dispatch(todoAction.selectItem(todo));}
     }
 };
 
 class ReduxInReact extends Component{
     constructor(props){
         super(props);
-        console.info("Linking the properties ",deleteTodo);
+        console.info("Linking the properties ");
     }
     submitTodo(){
         var itemObj = {title: this.refs.todoTitle.value};
         this.props.createToDo(itemObj);
         this.refs.todoTitle.value = "";
     }
+    deleteTodo({deleteId}){
+        var itemObj = {deleteId: deleteId};
+        this.props.removeToDoItem(itemObj);
+    }
+    selectTodo({selectId}){
+        var itemObj = {selectId: selectId}
+        this.props.selectTodoItem(itemObj);
+    }
     render(){
-        console.info("deleteTodo    ",deleteTodo);
         return (
             <div className='container rdxRctContent'>
                 <h3> Redux integration in React Applications </h3>
@@ -50,19 +60,27 @@ class ReduxInReact extends Component{
                 <div className='row todoContainer'>
                     <div className='col-sm-6 todoComponent'>
                         <div className='spacer'></div>
-                        <input type='text' placeholder='New Todo Item' ref='todoTitle' className='todoTitle'/>
+                        <input type='text' placeholder='New Todo' ref='todoTitle' className='todoTitle'/>
                         <button onClick={(event) => this.submitTodo()} className='btn btn-sm btn-info addItem'> Add </button>
-                        {(this.props.todos !== undefined) ? (
+                        {(this.props.todos.length !== 0) ? (
                             <ul>
                                 {this.props.todos.map((b, i) => <li className={i%2 === 0? "even":"odd"} key={i}>
                                     <span>{b.title}
-                                        <i className="glyphicon glyphicon-trash pull-right"></i>
-                                        <i className="glyphicon glyphicon-ok pull-right"></i>
+                                        <i onClick={(event) => this.deleteTodo({deleteId:b.todoItemId})} className="glyphicon glyphicon-trash pull-right"></i>
+                                        <i onClick={(event) => this.selectTodo({selectId:b.todoItemId})} className={b.selected === true ? "glyphicon glyphicon-ok pull-right showSelected":"glyphicon glyphicon-ok pull-right"}></i>
                                     </span>
                                 </li> )}
                             </ul>
                         ):(<ul><li>No Todos</li></ul>)}
                         <div className='spacer'></div>
+                    </div>
+                    <div className='col-sm-6'>
+                        <div className='col-sm-12 todoContentText'>
+                            <h4> Overview - Todo Implementation </h4>
+                            <p>This is a sample implementation to understand the usage of Redux in React.
+                            In this sample the user can add new todo items. Delate it and can select the one
+                            of his choice.</p>
+                        </div>
                     </div>
                 </div>
             </div>
